163583d151deb6a4d1b810337a1e73a9a4c4f686
dai-pop <114280166+dai-pop@users.noreply.github.com>
2023-01-02 22:25:08 +0900

    -

--- parse_dict/eijiro.py
+++ parse_dict/eijiro.py
@@ -1,6 +1,7 @@
 import re
 
 def deal_doublechar( forms ):
+    """colo(u)r を color と colour にする"""
     ret = []
     for form in forms:
         m = re.match(r"^(?P<head>\w+)\((?P<doublestr>\w+)\)(?P<tail>\w+)$", form)
@@ -13,17 +14,16 @@ def deal_doublechar( forms ):
     return ret
 
 
-def add_data( base, forms, description, number, out ):
+def add_data(base, level, forms, description, out):
     a = {}
     a['base'] = base
+    a['level'] = level
     a['forms'] = forms
     a['desc'] = description
-    a['number'] = number
-    out.append( a )
+    out.append(a)
 
 
-
-def get_dic( words ):
+def get_dic(src_words):
 
     alldata = []
 
@@ -44,28 +44,40 @@ def get_dic( words ):
 
         description = re.sub( r'■.*', '', description )
 
+        m = re.search(r"【レベル】(?P<level>\d+)", line)
+        if m and m.group("level"):
+            level = int(m.group("level"))
+        else:
+            level = 0
+
         m = re.search(r"【変化】(?P<infected>.*?)[【\n]", line)
         if m:
             infected_forms = deal_doublechar( re.findall( r'[a-zA-Z\(\)]+', m.group('infected') ) )
 
         if (prev_word == word):
             prev_description += "\n" + description
+            prev_level = max(prev_level, level)
         else:
-            if prev_word in words:
-                number = words.index(prev_word) + 1
+            if prev_word in src_words:
+                number = src_words.index(prev_word) + 1
                 infected_forms.insert( 0, prev_word )
-                add_data( prev_word, infected_forms, prev_description, number, alldata )
+                add_data( prev_word, prev_level, infected_forms, prev_description, alldata )
 
             prev_word = word
             prev_description = description
+            prev_level = 0
             infected_forms = []
 
-    if ( prev_word in words ):
-        number = words.index(prev_word) + 1
+    if ( prev_word in src_words):
         infected_forms.insert( 0, prev_word )
-        add_data( prev_word, infected_forms, prev_description, number, alldata )
+        add_data( prev_word, prev_level, infected_forms, prev_description, alldata )
 
     source.close()
 
     return alldata
 
+def main():
+    print( get_dic(["love", "death"]) )
+
+if __name__ == "__main__":
+    main()
